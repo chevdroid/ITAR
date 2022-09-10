224cbf01b8ddccbfb33f8590f94b48e41bc12d88
chevdroid <chevdroid@gmail.com>
2022-09-10 23:14:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flask:flask@localhost/microblog'.replace(
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
